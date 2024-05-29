9327ff5641d856167c7a0fdbfd757e5e43befbf5
Hossain <arafat.kd99@gmail.com>
2024-05-29 19:02:57 +0100

    Update SessionsController for api

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,2 +1,14 @@
-class Users::SessionsController < ApplicationController
+class Users::SessionsController < devise::ApplicationController
+  respond_to :json
+
+  private
+
+  def respond_with(resource, _opts = {})
+    render json: resource
+  end
+
+  def respond_to_on_destroy
+    head :no_content
+  end
+
 end
